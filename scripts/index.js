1749220f624dd2d60d2381592a98915814d57ef5
refresh669 <vlad27171@gmail.com>
2023-03-14 19:12:11 +0200

    fix2

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,34 @@
 'use strict'
+
+// 1
+
+let number = +prompt(`Name a number greater than 4 and less than 8`)
+if (number === 5 || number === 6 || number === 7) {
+    alert(`yes! =)`);
+}
+else {
+    alert(`No! =(`);
+}
+
+
+//2
+
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
+
+switch(true) {
+    case numOrStr === null:
+        console.log('вы отменили');
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
+
