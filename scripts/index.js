f6f3c8571265b85c6fbb49de21f6674c0f8f0a3a
refresh669 <vlad27171@gmail.com>
2023-03-14 14:58:54 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,33 @@
 'use strict'
+
+// Task# 1
+
+let example1 = Boolean(Number(`10`)) + 1;
+console.log(example1)
+let example2 = `sub` + Number(false);
+console.log(example2)
+let example3 = 16  *  `      91    `
+console.log(example3)
+let example4 = true-70
+console.log(example4)
+let example5 = Number(1 + String(1)) + 1
+console.log(example5)
+
+// Task# 2
+
+ const operator = prompt('Enter the number of hours ');
+ const result = operator * 3600
+ alert (`${result}`)
+
+// Task# 3
+
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
+
+
