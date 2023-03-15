207943b6d0bb8b1bdb2f13934cd2e6450f4c904a
refresh669 <vlad27171@gmail.com>
2023-03-15 11:22:25 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 'use strict'
+
+const old = +prompt(`Сколько вам лет?`)
+let answer
+
+switch (old) {
+    case 0:
+        answer = 'Вам не исполнилось даже года)'
+        break
+    case 1:
+        answer = 'Вам 1 год'
+        break
+    case 2:
+        answer = 'Вам ' + old + ' года'
+        break
+    case 3:
+        answer = 'Вам ' + old + ' года'
+        break
+    case 4:
+        answer = 'Вам ' + old + ' года'
+        break
+    default:
+        answer = 'Вам ' + old + ' лет'
+
+}
+
+alert(answer)
