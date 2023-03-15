82d8de1157c582f3c0ae501597632a754967eca0
refresh669 <vlad27171@gmail.com>
2023-03-15 13:40:43 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const x = 4;
+const y = 9;
+const r = 10;
+
+const Hypotenuse = Math.sqrt(x * x + y * y);
+const result = Hypotenuse <= r ? "Точка лежит внутри окружности" : "Точка за пределами окружности";
+
+alert(result);
