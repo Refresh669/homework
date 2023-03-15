00629c445b9f5875a17ae09dd9e0fe652a00e99d
refresh669 <vlad27171@gmail.com>
2023-03-15 11:48:20 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 'use strict'
+
+
+// Функция, которая проверяет, равны ли два числа
+function areNumbersEqual(num1, num2) {
+    if (num1 === num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Функция, которая проверяет, больше ли сумма двух чисел, чем 10
+function isSumGreaterThan10(num1, num2) {
+    if ((num1 + num2) > 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Функция, которая проверяет, отрицательное ли число
+function isNumberNegative(num) {
+    if (num < 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
