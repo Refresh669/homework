637d87dcb7dbef2aae98d298a4b6df732896ec39
refresh669 <vlad27171@gmail.com>
2023-03-21 15:08:52 +0200

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-'use strict'
+'use strict';
+
+const num = prompt(`Введите число`);
+const degree = prompt(`Введите степень`);
+
+function operation(num, degree = 1) {
+
+    return Math.pow(num, degree);
+}
+
+const result = operation(num, degree);
+console.log(result);
