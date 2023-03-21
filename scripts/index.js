98db440ddb73c7ff18ddda7b2510d1718e98de8e
refresh669 <vlad27171@gmail.com>
2023-03-21 19:22:36 +0200

    2

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
+
+
 const num = prompt(`Введите число`);
 const degree = prompt(`Введите степень`);
 
 function operation(num, degree = 1) {
-
+    if (isNaN(num) || isNaN(degree)){
+        alert(`some error`)
+    }
     return Math.pow(num, degree);
 }
-
 const result = operation(num, degree);
-console.log(result);
+alert(result)
