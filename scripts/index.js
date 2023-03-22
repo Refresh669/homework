627dadee22d6f40e596fb3c2d8f5817eaccad863
refresh669 <vlad27171@gmail.com>
2023-03-22 14:17:00 +0200

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const number = prompt("Введите число:");
+
+if (number <= 1) {
+    console.log("NaN");
+} else {
+    let divider = 2;
+    while (number % divider !== 0) {
+        divider++;
+    }
+    if (number == divider) {    // есть ставить строгое сравнение то кол ломаеться, if не выполняеться
+        alert(`Число ` + number + ` простое и делиться только на смого себя либо 1`)
+    }
+    else {
+        alert(`Наименьший делитель числа ${number} равен ${divider}`);
+    }
+}
