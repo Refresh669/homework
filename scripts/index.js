f181dd00f18de2a09e9b66f980147c83c4c2ff8c
refresh669 <vlad27171@gmail.com>
2023-03-22 13:48:17 +0200

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const number = +prompt("Введите число: ");
+let isPrime = true;
+
+if (number === 1) {
+    isPrime = false;
+} else {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+
+console.log(isPrime);
