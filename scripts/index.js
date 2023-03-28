39d8c84c1fcec13aedfa3d20cdbf33dc230e0fd9
refresh669 <vlad27171@gmail.com>
2023-03-29 00:59:55 +0300

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ else  console.log(`x не больше, чем y`)
 
 // 2
 
-let num = +prompt("Введите число");
+const num = +prompt("Введите число");
 
 if (num % 2 === 0) {
     console.log("Число " + num + " четное");
@@ -21,8 +21,8 @@ if (num % 2 === 0) {
 
 // 3
 
-let num2 = +prompt("Введите целое число:");
-let num2Length = num2.length;
+const num2 = +prompt("Введите целое число:");
+const num2Length = num2.length;
 
 if (num2 < 0) {
     console.log("Число " + num2 + " отрицательное");
@@ -42,9 +42,9 @@ if (num2Length === 1) {
 
 // 4
 
-let a = +prompt("Введите целое число 1");
-let b = +prompt("Введите целое число 2");
-let c = +prompt("Введите целое число 3");
+const a = +prompt("Введите целое число 1");
+const b = +prompt("Введите целое число 2");
+const c = +prompt("Введите целое число 3");
 
 if (a > b && a > c) alert(`Наибольшее число ` + a)
 else if (b > a && b > c) alert(`Наибольшее число ` + b)
@@ -53,9 +53,9 @@ else alert(`Наибольшее число ` + c)
 
 // 5
 
-let sideA = +prompt("Введите длину 1 стороны треугольника");
-let sideB = +prompt("Введите длину 2 стороны треугольника");
-let sideC = +prompt("Введите длину 3 стороны треугольника");
+const sideA = +prompt("Введите длину 1 стороны треугольника");
+const sideB = +prompt("Введите длину 2 стороны треугольника");
+const sideC = +prompt("Введите длину 3 стороны треугольника");
 
 if (((sideA + sideB) > sideC) && ((sideA + sideC) > sideB) && ((sideB + sideC) > sideA))  alert(`Такой треугольник может существовать`)
 else alert(`Такой треугольник не может существовать`)
