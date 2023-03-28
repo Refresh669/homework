b14906929bfcaf2bfabb9142e9e15a86f206d27b
refresh669 <vlad27171@gmail.com>
2023-03-29 01:02:38 +0300

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const x = 4;
 const y = 9;
 const r = 10;
 
-const Hypotenuse = Math.sqrt(x * x + y * y);
-const result = Hypotenuse <= r ? "Точка лежит внутри окружности" : "Точка за пределами окружности";
+const hypotenuse = Math.sqrt(x * x + y * y);
+const result = hypotenuse <= r ? "Точка лежит внутри окружности" : "Точка за пределами окружности";
 
 alert(result);
