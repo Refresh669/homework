b527c3d2caa33195b06f814625d5ffd89b653d34
refresh669 <vlad27171@gmail.com>
2023-03-28 23:11:01 +0300

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 'use strict'
+
+function getPositiveNumbers(arr) {
+    const positiveArr = [];
+
+    if (arr.length === 0) {
+        return "Массив пустой";
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positiveArr.push(arr[i]);
+        }
+    }
+
+    if (positiveArr.length > 0) {
+        return positiveArr;
+    } else {
+        return null;
+    }
+}
+
+const arr = [1, 2, 3, -1, -2, -3];
+const positiveArr = getPositiveNumbers(arr);
+
+console.log(positiveArr);
+
