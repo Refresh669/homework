03d5120c67c22cb43dfdefbcccabaea5bf22e70d
refresh669 <vlad27171@gmail.com>
2023-03-28 23:59:02 +0300

    ff

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
+// 'use strict'
+//
+// function calculate(liters) {
+//     const potatoesPerLiter = 4;
+//     const weightPerPotato = 75; // в граммах
+//     const pricePerKg = 13;
+//     return (liters * potatoesPerLiter * weightPerPotato / 1000 ) * pricePerKg;
+// }
+// const borschtLiters = 48;
+// const totalPrice = Math.ceil(calculate (borschtLiters));
+// console.log(`Стоимость покупки картошки для приготовления ${borschtLiters} литров борща: ${totalPrice} грн`);
+
+
+
+
 'use strict'
+
+function calculate(liters) {
+    const potatoesPerLiter = 4;
+    const weightPerPotato = 75;
+    const pricePerKg = 13;
+    return Math.ceil(liters * potatoesPerLiter * weightPerPotato / 1000 * pricePerKg);
+}
+const borschtLiters = 48;
+console.log(`Стоимость покупки картошки для приготовления ${borschtLiters} литров борща: ${calculate(borschtLiters)} грн`);
