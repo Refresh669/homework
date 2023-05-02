cd899a6f3c6131636f06de07c05b124078e10957
refresh669 <vlad27171@gmail.com>
2023-05-02 20:54:01 +0300

    123

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,37 @@
 'use strict'
+
+'use strict';
+
+//Task #1
+function factorial(n) {
+    if (n === 1 || n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(3))
+
+//Task #2
+
+function pow(num, deg) {
+    if(deg === 1) {
+        return num
+    }
+
+    return num + pow(num, deg - 1);
+}
+
+console.log(pow(5, 2));
+
+//Task #3
+
+function sum(a, b) {
+    if (b === 0) {
+        return a;
+    }
+    return sum(a + 1, b - 1);
+
+}
+
+console.log(sum(6,4))
